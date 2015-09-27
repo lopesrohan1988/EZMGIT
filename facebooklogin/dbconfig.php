e621e652700a42f055cc111fa403e21bd5671c01
lopesrohan1988 <lopesrohan1988@gmail.com>
2015-09-27 15:31:19 +0530

    s

--- facebooklogin/dbconfig.php
+++ facebooklogin/dbconfig.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'EZM');    // DB username
-define('DB_PASSWORD', 'welcome1');    // DB password
+define('DB_SERVER', '127.8.118.130:3306');
+define('DB_USERNAME', 'admincRRFhGq');    // DB username
+define('DB_PASSWORD', 'm9HYPyI14mYW');    // DB password
 define('DB_DATABASE', 'EZ');      // DB name
 $connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_DATABASE) or die(mysqli_connect_error());
 $database = mysql_select_db(DB_DATABASE) or die( "Unable to select database");
